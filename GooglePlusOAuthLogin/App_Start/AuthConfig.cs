f0a3ecd148bec82f769744e89e1cadd185df2282
Jerrie Pelser <jerrie.pelser@gmail.com>
2013-03-10 14:22:02 +0700

    Initial commit

---
+++ GooglePlusOAuthLogin/App_Start/AuthConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using GooglePlusOAuthLogin.Models;
+
+namespace GooglePlusOAuthLogin
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            OAuthWebSecurity.RegisterClient(new GooglePlusClient("808748037769.apps.googleusercontent.com", "GwaMKxJSA6v-9ItP4iAdRZZ5"), "Google+", null);
+        }
+    }
+}
